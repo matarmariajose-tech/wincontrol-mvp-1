cb81731141a65bcc887fd6670bcbcadd438934ef
tomibonso <tomas.bonsignore@gmail.com>
2026-04-23 18:22:45 -0300

    fix: database for railway

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -8,12 +8,19 @@ import { Property } from "../properties/property.entity";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  synchronize: true, // solo dev
+  ...(process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }),
+  synchronize: true,
   logging: false,
   entities: [User, Visit, Lead, Property],
 });
