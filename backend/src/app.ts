e6eb1f3dd8f444b6e39c49b926f7e054d3da18f1
tomibonso <tomas.bonsignore@gmail.com>
2026-04-23 19:00:16 -0300

    fix: more changes in db for railway

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,10 +28,9 @@ AppDataSource.initialize()
   .then(() => {
     console.log("DB connected");
 
-    const PORT = process.env.PORT || 3000;
-
-    app.listen(PORT, () => {
-      console.log("Server running on port ${PORT}");
+    const port = Number(process.env.PORT) || 3000;
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`Server running on port ${port}`);
     });
   })
   .catch((error) => {
