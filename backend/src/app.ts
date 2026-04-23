29918dda46d9530ef4b5e4f1fcdd8f35d0241ab8
tomibonso <tomas.bonsignore@gmail.com>
2026-04-23 16:15:00 -0300

    fix: add port for railway

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -28,8 +28,10 @@ AppDataSource.initialize()
   .then(() => {
     console.log("DB connected");
 
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => {
+      console.log("Server running on port ${PORT}");
     });
   })
   .catch((error) => {
