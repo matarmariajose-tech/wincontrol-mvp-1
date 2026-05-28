55e5b63a4f9f8ca14a26611c2aa2c96f955a112e
tomibonso <tomas.bonsignore@gmail.com>
2026-05-28 12:37:31 -0300

    fix: url

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   //API_URL: "https://wincontrol-mvp-production.up.railway.app"
-  API_URL: "http://localhost:3000"
+  //API_URL: "http://localhost:3000"
+  API_URL: "https://wincontrol-backend.onrender.com"
 };
